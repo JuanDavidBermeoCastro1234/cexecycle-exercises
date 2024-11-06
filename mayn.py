b18bbb417bca94a52c7210d0b03874a899286fb0
JuanDavidBermeoCastro <186082659+JuanDavidBermeoCastro@users.noreply.github.com>
2024-11-06 12:40:47 -0500

    Tabla de multiplicar

--- mayn.py
+++ mayn.py
@@ -1,13 +1,8 @@
-# Ask the user for two integers
-num1 = int(input("Enter the first integer: "))
-num2 = int(input("Enter the second integer: "))
 
-# Make sure num1 is the smaller number and num2 is the larger one
-if num1 > num2:
-    num1, num2 = num2, num1  # Swap the values if needed
+for i in range(1, 11):
+  
+    for j in range(1, 11):
+     
+        print(f"{i * j:3}", end=" ")
 
-# Calculate the sum of the numbers between num1 and num2
-sum_result = sum(range(num1 + 1, num2))
-
-# Display the result
-print(f"The sum of the numbers between {num1} and {num2} is: {sum_result}")
+    print()
