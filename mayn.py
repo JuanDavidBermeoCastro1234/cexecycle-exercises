7da6b432f2ce77e08236de90d207d92ff9b06c1c
JuanDavidBermeoCastro <186082659+JuanDavidBermeoCastro@users.noreply.github.com>
2024-11-06 12:43:04 -0500

    tiempo de viaje

--- mayn.py
+++ mayn.py
@@ -1,7 +1,15 @@
 
-number = int(input("Enter an integer: "))
+total_minutes = 0
 
-print(f"The divisors of {number} are:")
-for i in range(1, number + 1):
-    if number % i == 0:
-        print(i)
+while True:
+    segment = int(input("Segment duration: "))
+    
+    if segment == 0:
+        break
+    
+    total_minutes += segment
+
+hours = total_minutes // 60
+minutes = total_minutes % 60
+
+print(f"Total travel time: {hours}:{minutes:02d} hours")
