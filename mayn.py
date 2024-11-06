1875514f7846e806e4f1fff5e1d3485f1aa46e45
JuanDavidBermeoCastro <186082659+JuanDavidBermeoCastro@users.noreply.github.com>
2024-11-06 12:47:08 -0500

    dibujos de arteriscos

--- mayn.py
+++ mayn.py
@@ -1,15 +1,5 @@
 
-total_minutes = 0
+height = int(input("Height: "))
 
-while True:
-    segment = int(input("Segment duration: "))
-    
-    if segment == 0:
-        break
-    
-    total_minutes += segment
-
-hours = total_minutes // 60
-minutes = total_minutes % 60
-
-print(f"Total travel time: {hours}:{minutes:02d} hours")
+for i in range(1, height + 1):
+    print("*" * i)
